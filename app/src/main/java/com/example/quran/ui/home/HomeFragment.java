565fac3133a0be88ba80137c399acf02c88c664f
Roohollah419 <Roohollah419@gmail.com>
2026-02-24 17:26:25 +0330

    Fix home fragment to show the order of ayah address correctly

--- app/src/main/java/com/example/quran/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/quran/ui/home/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.example.quran.ui.home;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.TypefaceSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
@@ -94,25 +99,86 @@ public class HomeFragment extends BaseFragment {
                         boolean isArabic = settingsManager.isArabicLanguage();
                         String surahName;
                         String ayahNumberDisplay;
+                        String ayahInfo;
+
+                        Typeface arabicTypeface = ResourcesCompat.getFont(requireContext(), R.font.uthmantaha);
 
                         if (isArabic) {
                             // Use custom calligraphy font for Arabic surah names
                             surahName = SurahFontHelper.getCharacter(surah.getNumber());
                             ayahNumberDisplay = ArabicNumeralConverter.convert(ayah.getAyahNumber());
-                            tvAyahInfo.setTypeface(SurahFontHelper.getTypeface(requireContext()));
                             tvAyahInfo.setTextSize(14 * settingsManager.getFontSizeMultiplier() * 1.5f);
+                            tvAyahInfo.setTextDirection(android.view.View.TEXT_DIRECTION_RTL);
+
+                            // Force RTL display: wrap entire string in RLE (Right-to-Left Embedding)
+                            // Order in string: surah name, space, brackets with number
+                            // Display will be RTL: surah name (right), number (left)
+                            String ayahNumberPart = " ﴿" + ayahNumberDisplay + "﴾";
+                            String fullText = "\u202B" + surahName + ayahNumberPart + "\u202C";
+
+                            SpannableString spannableString = new SpannableString(fullText);
+
+                            // Apply calligraphy font to surah name (skip RLE character)
+                            Typeface surahTypeface = SurahFontHelper.getTypeface(requireContext());
+                            spannableString.setSpan(new CustomTypefaceSpan(surahTypeface),
+                                1, 1 + surahName.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                            // Apply Uthman Taha font to ayah number and brackets
+                            spannableString.setSpan(new CustomTypefaceSpan(arabicTypeface),
+                                1 + surahName.length(), fullText.length() - 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                            tvAyahInfo.setText(spannableString);
                         } else {
                             surahName = surah.getNameEnglish();
+                            // Use Western numerals for English
                             ayahNumberDisplay = String.valueOf(ayah.getAyahNumber());
-                            tvAyahInfo.setTypeface(android.graphics.Typeface.DEFAULT);
                             tvAyahInfo.setTextSize(14);
-                        }
+                            tvAyahInfo.setTextDirection(android.view.View.TEXT_DIRECTION_LTR);
 
-                        String ayahInfo = String.format("%s (%s)", surahName, ayahNumberDisplay);
-                        tvAyahInfo.setText(ayahInfo);
+                            // LTR format: surah name (default font) then ayah number with brackets (Uthman Taha font)
+                            // In LTR context with Western numerals, brackets display as ﴾number﴿
+                            String ayahNumberPart = " ﴾" + ayahNumberDisplay + "﴿";
+                            String fullText = surahName + ayahNumberPart;
+
+                            SpannableString spannableString = new SpannableString(fullText);
+
+                            // Apply Uthman Taha font to ayah number and brackets
+                            spannableString.setSpan(new CustomTypefaceSpan(arabicTypeface),
+                                surahName.length(), fullText.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                            tvAyahInfo.setText(spannableString);
+                        }
                     }
                 });
             }
         });
+    }
+
+    /**
+     * Custom TypefaceSpan that properly applies custom fonts.
+     */
+    private static class CustomTypefaceSpan extends TypefaceSpan {
+        private final Typeface newTypeface;
+
+        public CustomTypefaceSpan(Typeface typeface) {
+            super("");
+            this.newTypeface = typeface;
+        }
+
+        @Override
+        public void updateDrawState(android.text.TextPaint paint) {
+            applyCustomTypeFace(paint, newTypeface);
+        }
+
+        @Override
+        public void updateMeasureState(android.text.TextPaint paint) {
+            applyCustomTypeFace(paint, newTypeface);
+        }
+
+        private void applyCustomTypeFace(android.graphics.Paint paint, Typeface tf) {
+            if (tf != null) {
+                paint.setTypeface(tf);
+            }
+        }
     }
 }
